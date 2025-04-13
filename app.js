c5d5f941ec51b91131b81b5c4a3912d3a4e28474
pvarthya <pvarthya@regis.edu>
2025-04-13 15:09:21 -0400

    Change name to Pavan

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is Pavan Varthya');
+	  res.end('Hello World. My name is Pavan');
 });
 
 server.listen(port, hostname, () => {
